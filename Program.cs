2f5c8a2addc53aa036347b35f45fea23a1e098f2
Christian Kapplmüller <christian.kapplmueller@students.fh-hagenberg.at>
2018-02-20 11:04:16 +0100

    reproduced issue

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
 
 namespace Azurite157Repro
 {
@@ -6,7 +8,21 @@ namespace Azurite157Repro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Task.Run(async () =>
+            {
+                var account = CloudStorageAccount.Parse("BlobEndpoint=http://localhost:10000/devstoreaccount1;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==");
+                var client = account.CreateCloudBlobClient();
+
+                var sourceContainer = client.GetContainerReference("source-container");
+                await sourceContainer.CreateIfNotExistsAsync();
+                var sourceBlob = sourceContainer.GetBlockBlobReference("the-blob");
+                await sourceBlob.UploadTextAsync("test");
+
+                var targetContainer = client.GetContainerReference("target-container");
+                await targetContainer.CreateIfNotExistsAsync();
+                var targetBlob = targetContainer.GetBlockBlobReference(sourceBlob.Name);
+                await targetBlob.StartCopyAsync(sourceBlob);
+            }).Wait();
         }
     }
 }
