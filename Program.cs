beab649c791eacef15f393f2d93e6131209ab0c0
Christian Kapplmüller <christian.kapplmueller@students.fh-hagenberg.at>
2018-02-20 10:36:57 +0100

    initial project setup

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Azurite157Repro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
